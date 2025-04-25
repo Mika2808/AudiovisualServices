3d6595aa91ec7461d6a9929f8646460f4a073ae9
Mika2808 <mikabala2003@gmail.com>
2025-04-25 19:57:27 +0200

    Starting the project

--- Project/project.py
+++ Project/project.py
@@ -3,13 +3,36 @@ import customtkinter
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-
-        self.title("Graphical Interface for FFMPEG")
+        self.title("Mikołaj Kabała")
         self.geometry("800x500")
+        customtkinter.set_widget_scaling(1.3)  # widget dimensions and text size
+        #self.starting_frame = StartingFrame(self)
+
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
+        
+        
+        self.start_frame = customtkinter.CTkLabel(self, text="Welcome to graphical interface for FFMPEG", fg_color="gray30", corner_radius=10)
+        self.start_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
 
-        self.start_frame = CheckboxFrame(self, title="Hello", values= ["value1", "value2"])
+        self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
+        self.button.grid(row=1, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+        
+    def button_callback(self):
+        print("Button")
+        
+        
+
+    
+class StartingFrame(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        
+        
+        self.start_frame = customtkinter.CTkLabel(self, text="Welcome to graphical interface for FFMPEG", fg_color="gray30", corner_radius=10)
         self.start_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsew")
 
         self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
@@ -18,28 +41,6 @@ class App(customtkinter.CTk):
     def button_callback(self):
         print("Button")
 
-class CheckboxFrame(customtkinter.CTkFrame):
-    def __init__(self, master, title, values):
-        super().__init__(master)
-        self.grid_columnconfigure(0, weight=1)
-        self.values = values
-        self.title = title
-        self.checkboxes = []
 
-        self.title = customtkinter.CTkLabel(self, text=self.title, fg_color="gray30", corner_radius=6)
-        self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
-
-        for i, value in enumerate(self.values):
-            checkbox = customtkinter.CTkCheckBox(self, text=value)
-            checkbox.grid(row=i+1, column=0, padx=10, pady=(10, 0), sticky="ew")
-            self.checkboxes.append(checkbox)
-
-    def get(self):
-        checked_checkboxes = []
-        for checkbox in self.checkboxes:
-            if checkbox.get() == 1:
-                checked_checkboxes.append(checkbox.cget("text"))
-        return checked_checkboxes
-    
 app = App()
 app.mainloop()
